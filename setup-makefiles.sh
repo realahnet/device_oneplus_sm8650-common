7fb8133d14d708552d5d3cddb4ad7985d8cb9e4f
John Galt <johngaltfirstrun@gmail.com>
2026-03-06 11:44:32 +0500

    sm8650-common: revert to prebuilt audio
    Some OP12 variants on stock do not use lvacfs, and when used here
    counterintuitively causes mic volume to be too low. Stock hal has some
    behavior which resolves this, so when this behavior is implemented,
    this will be reverted later.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -73,14 +73,12 @@ function lib_to_package_fixup_vendor_variants() {
             ;;
         libagmclient | \
             libpalclient | \
-            libar-pal | \
             libar-acdb | \
             libar-gsl | \
+            libats | \
             liblx-osal | \
             liblx-ar_util | \
             vendor.qti.hardware.AGMIPC@1.0-impl | \
-            libats | \
-            libagm | \
             libwpa_client) ;;
         *)
             return 1
