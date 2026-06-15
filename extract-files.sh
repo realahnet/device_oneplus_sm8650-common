fa339fec71b2169d6258304182ec9bf87a0bb605
realahnet <syedmusicrealise@gmail.com>
2026-06-15 22:40:46 +0500

    sm8650-common: Apply clear symbol elf patches for camera blobs with patchelf 0.18
    * Older elf fails to patch the libs and throws:
    
    stat: no such file or directory

--- extract-files.sh
+++ extract-files.sh
@@ -190,24 +190,24 @@ function blob_fixup() {
             case "${DEVICE}" in
             giulia | giuliac)
                 [ "$2" = "" ] && return 0
-                "${PATCHELF}" --clear-symbol-version "AHardwareBuffer_acquire" "${2}"
-                "${PATCHELF}" --clear-symbol-version "AHardwareBuffer_allocate" "${2}"
-                "${PATCHELF}" --clear-symbol-version "AHardwareBuffer_describe" "${2}"
-                "${PATCHELF}" --clear-symbol-version "AHardwareBuffer_lock" "${2}"
-                "${PATCHELF}" --clear-symbol-version "AHardwareBuffer_lockPlanes" "${2}"
-                "${PATCHELF}" --clear-symbol-version "AHardwareBuffer_release" "${2}"
-                "${PATCHELF}" --clear-symbol-version "AHardwareBuffer_unlock" "${2}"
+                "${PATCHELF_0_18}" --clear-symbol-version "AHardwareBuffer_acquire" "${2}"
+                "${PATCHELF_0_18}" --clear-symbol-version "AHardwareBuffer_allocate" "${2}"
+                "${PATCHELF_0_18}" --clear-symbol-version "AHardwareBuffer_describe" "${2}"
+                "${PATCHELF_0_18}" --clear-symbol-version "AHardwareBuffer_lock" "${2}"
+                "${PATCHELF_0_18}" --clear-symbol-version "AHardwareBuffer_lockPlanes" "${2}"
+                "${PATCHELF_0_18}" --clear-symbol-version "AHardwareBuffer_release" "${2}"
+                "${PATCHELF_0_18}" --clear-symbol-version "AHardwareBuffer_unlock" "${2}"
 	    esac
             ;;
         odm/lib64/libarcsoft_high_dynamic_range_v4.so)
             case "${DEVICE}" in
             giulia | giuliac)
                 [ "$2" = "" ] && return 0
-                "${PATCHELF}" --clear-symbol-version "remote_handle_close" "${2}"
-                "${PATCHELF}" --clear-symbol-version "remote_handle_invoke" "${2}"
-                "${PATCHELF}" --clear-symbol-version "remote_handle_open" "${2}"
-                "${PATCHELF}" --clear-symbol-version "remote_register_buf_attr" "${2}"
-                "${PATCHELF}" --clear-symbol-version "remote_register_buf" "${2}"
+                "${PATCHELF_0_18}" --clear-symbol-version "remote_handle_close" "${2}"
+                "${PATCHELF_0_18}" --clear-symbol-version "remote_handle_invoke" "${2}"
+                "${PATCHELF_0_18}" --clear-symbol-version "remote_handle_open" "${2}"
+                "${PATCHELF_0_18}" --clear-symbol-version "remote_register_buf_attr" "${2}"
+                "${PATCHELF_0_18}" --clear-symbol-version "remote_register_buf" "${2}"
             esac
             ;;
         *)
